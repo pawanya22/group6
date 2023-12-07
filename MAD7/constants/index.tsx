68a946b8bbacaeb543a5039f1cd46d950ad944b3
Nalina2001 <144620106+Nalina2001@users.noreply.github.com>
2023-12-07 15:54:29 +0530

    API folders

---
+++ MAD7/constants/index.tsx
@@ -0,0 +1,20 @@
+export const apikeys ='movie db key';
+
+export const MovieData=[
+    {
+
+    }
+]
+
+export const Actordata=[
+    {
+        name:'',
+        birthplace:'',
+        birthday:'',
+        gender:'',
+        knownfor:'',
+        popularity:'',
+        biography:'',
+
+    }
+]
